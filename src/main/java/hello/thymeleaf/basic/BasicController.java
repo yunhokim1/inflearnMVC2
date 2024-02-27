f86d0c89b3496c933455e721c4d3ade33ebf9798
kyh <kyh@tigensoft.co.kr>
2024-02-27 15:27:13 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 //섹션1까지

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -116,6 +116,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+
+        model.addAttribute("user", new User("userA", 20));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 
     private void addUsers(Model model) {
 
